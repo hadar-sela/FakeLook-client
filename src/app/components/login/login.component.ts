89990783df001b7dbe4e6e0b510f0f3eccc06e1e
no <no@gmail.com>
2022-03-15 16:17:15 +0200

    client

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
@@ -18,22 +19,11 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
-    console.log("You enter the username:");
-    console.log(this.username);
-    console.log("You enter the password:")
-    console.log(this.password);
-    this.LoginService.getUser(this.username).subscribe((result)=>{
-      console.log(result);
-      if(!result){
-        this.message = "Incorrect username or password";
-      }
-      if(result.password != this.password){
-        this.message = "Incorrect username or password";
-      }
-      else{
-        this.message = "";
-      }
-      
+    const newUser= {} as User;
+    newUser.username=this.username;
+    newUser.password=this.password;
+    this.LoginService.getUser(newUser).subscribe((result)=>{
+      console.log(result);      
     })
   }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -11,9 +11,10 @@ export class LoginService {
 
   constructor(private http:HttpClient) { }
 
-  getUser(username: string): Observable<User>{
-    const url = environment.usersUrl + "/GetByUsername/" + username;
-    return this.http.get<User>(url);
+  getUser(user: User): Observable<User>{
+    //const url = environment.usersUrl + "/GetByUsername/" + username;
+    //return this.http.get<User>(url);
+    return this.http.post<User>(environment.usersUrl+'/Login',user)
   }
 
 }
