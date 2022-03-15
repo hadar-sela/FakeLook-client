334370e3063b98aba4412575d52bb23c7d04e703
hadar-sela <101385170+hadar-sela@users.noreply.github.com>
2022-03-15 14:53:51 +0200

    Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+// const routes: Routes = [
+//   { path: 'heroes', component: HeroesComponent }
+// ];
+const routes: Routes = [
+  { path: '', component: LoginComponent }, 
+  { path: 'register', component: RegisterComponent}, 
+  { path: 'login', component: LoginComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
